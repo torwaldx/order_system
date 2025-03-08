d3b09293916406d0408564c47a5917809f38bb95
Maksim Levshin <levshin_maksim@list.ru>
2025-03-08 05:53:26 +0000

    add message history

---
+++ llm_api.py
@@ -0,0 +1,102 @@
+import os
+import streamlit as st
+
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate, FewShotPromptTemplate
+from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
+from langchain_core.chat_history import  BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_community.chat_message_histories.streamlit import StreamlitChatMessageHistory
+
+from db import get_products_str
+from few_shot import get_fewshot
+
+
+if not os.environ.get('OPENAI_API_KEY'):
+    os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
+
+llm = ChatOpenAI(
+    openai_api_base='https://api.proxyapi.ru/openai/v1',
+    model_name="gpt-4o-mini",
+    temperature=0.0,
+)
+
+def get_session_history() -> BaseChatMessageHistory:
+    return StreamlitChatMessageHistory()
+
+def debug_prompt(inputs):
+    print("=== Полный промпт ===")
+    for m in inputs.to_messages():
+      st.sidebar.markdown(m.pretty_repr())
+    print("=====================")
+    return inputs  # Пропускаем дальше без изменений
+
+def get_order_process_chain():
+    # Шаблон для подсказок
+    example_prompt = PromptTemplate.from_template(
+        "Запрос пользователя: {Question}\nРаспознанные товары: {Products_name}"
+    )
+
+    # Шаблон для блока few-shot
+    few_shot_prompt_template = FewShotPromptTemplate(
+        examples=[],
+        example_prompt=example_prompt,
+        prefix="Ниже приведены подсказки для неоднозначных запросов:",
+        suffix="",
+        input_variables=["user_request"]
+    )
+   
+    system_message = """Ты - умный ИИ помощник - помогаешь сформировать корзину в интернет-магазине.
+Если не удается определить подходящие товары - используй подсказки или предложи одну маленькую пиццу и напиток.
+Исходя из перечня доступных товаров и запроса пользователя , сформируй список товаров с указанием их количества.
+Если пользователь указал конкретное количество, используй его.
+Если количество не указано явно, оцени его на основании контекста.
+Если контекста недостаточно, укажи "1" по умолчанию.
+
+Формат ответа: JSON-объект с ключом "items", содержащим список словарей с полями "name" (название товара) и "quantity" (количество).
+Ответ должен быть строго JSON-объектом без пояснений и дополнительного текста.
+Пример ответа:
+Запрос пользователя: "Две больших пеперрони и 3 напитка"
+Ответ:
+{{
+    "items": [
+        {{"name": "Пепперони Large", "quantity": 2}},
+        {{"name": "Добрый Кола Small", "quantity": 3}}
+    ]
+}}
+
+{few_shot}
+
+Вот перечень доступных товаров (товар | тип | цена):
+{products}
+"""
+
+    prompt = ChatPromptTemplate([
+            ("system", system_message),
+            MessagesPlaceholder(variable_name="history", n_messages=5, optional=True),
+            ("human", "{user_request}"),
+    ])
+
+    chain = (
+        RunnablePassthrough.assign(
+            products=lambda _: get_products_str(),
+            few_shot=lambda inputs: few_shot_prompt_template.model_copy(
+                update={"examples": get_fewshot(inputs["user_request"])}
+            ).format(user_request=inputs["user_request"])
+        )
+        | prompt
+        | RunnableLambda(debug_prompt)  # Выводит полный промпт перед отправкой в LLM
+        | llm
+    )
+
+    rwmh = RunnableWithMessageHistory(
+        chain,
+        get_session_history,
+        input_messages_key="user_request",
+        history_messages_key="history",
+    )
+
+    return rwmh
+
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,114 +1,22 @@
 import streamlit as st
 import json
-import os
-
-from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate, FewShotPromptTemplate
-from langchain.chains import LLMChain
-
-from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
 
 import db
-import few_shot
+from llm_api import get_order_process_chain
 
-# Show title and description.
+
 st.title("💬 Чат бот для размещения заказов в пиццерии")
 
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-
-if not os.environ.get('OPENAI_API_KEY'):
-    os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
-
-llm = ChatOpenAI(
-    openai_api_base='https://api.proxyapi.ru/openai/v1',
-    model_name="gpt-4o-mini",
-    temperature=0.0,
-)
-
-
-def debug_prompt(inputs):
-    print("=== Полный промпт ===")
-    print(inputs.text)
-    print("=====================")
-    return inputs  # Пропускаем дальше без изменений
-
-def get_order_process_chain():
-    # Шаблон для подсказок
-    example_prompt = PromptTemplate.from_template(
-        "Запрос пользователя: {Question}\nРаспознанные товары: {Products_name}"
-    )
-
-    # Шаблон для few-shot подсказок
-    few_shot_prompt_template = FewShotPromptTemplate(
-        examples=[],
-        example_prompt=example_prompt,
-        prefix="Ниже приведены подсказки для неоднозначных запросов:",
-        suffix="",
-        input_variables=["user_request"]
-    )
-
-    # Основной шаблон для формирования запроса
-    get_products_template = """Ты - умный ИИ помощник - помогаешь сформировать корзину в интернет-магазине.
-    Если не удается определить подходящие товары - используй подсказки или предложи одну маленькую пиццу и напиток.
-    Исходя из перечня доступных товаров и запроса пользователя , сформируй список товаров с указанием их количества.
-    Если пользователь указал конкретное количество, используй его.
-    Если количество не указано явно, оцени его на основании контекста.
-    Если контекста недостаточно, укажи "1" по умолчанию.
-    Ты можешь выбирать товары только из перечня доступных товаров с ценами, никаких посторонних продуктов.
-
-    Формат ответа: JSON-объект с ключом "items", содержащим список словарей с полями "name" (название товара) и "quantity" (количество).
-    Ответ должен быть строго JSON-объектом без пояснений и дополнительного текста.
-    Пример ответа:
-    Запрос пользователя: "Две больших пеперрони и 3 напитка"
-    Ответ:
-    {{
-        "items": [
-            {{"name": "Пепперони Large", "quantity": 2}},
-            {{"name": "Добрый Кола Small", "quantity": 3}}
-        ]
-    }}
-
-    {few_shot}
-
-    Вот перечень доступных товаров (товар | тип | цена):
-    {products}
-
-    Текущий запрос пользователя: {user_request}
-    """
-
-    base_prompt = PromptTemplate.from_template(get_products_template)
-
-    
-    chain = (
-        RunnablePassthrough.assign(
-            products=lambda _: db.get_products_str(),
-            few_shot=lambda inputs: few_shot_prompt_template.copy(
-                update={"examples": few_shot.get_fewshot(inputs["user_request"])}
-            ).format(user_request=inputs["user_request"])
-        )
-        | base_prompt
-        # | RunnableLambda(debug_prompt)  # Выводит полный промпт перед отправкой в LLM
-        | llm
-    )
-    return chain
-
-
-
-# Create a session state variable to store the chat messages. This ensures that the
-# messages persist across reruns.
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Display the existing chat messages via `st.chat_message`.
+
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Create a chat input field to allow the user to enter a message. This will display
-# automatically at the bottom of the page.
+
 if prompt := st.chat_input("Просто напечатайте, что вы хотите"):
 
     # Store and display the current prompt.
@@ -122,7 +30,6 @@ if prompt := st.chat_input("Просто напечатайте, что вы х�
 
     order = db.get_products_with_prices(json.loads(answer.content))
     order_text = "\n\n".join([f"{item['name']} - {item['quantity']} x {item['price']}" for item in order['items']])
-
 
     # Stream the response to the chat using `st.write_stream`, then store it in 
     # session state.
